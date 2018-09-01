745989dda92e5231c3255b8672d1b561c6bc941b
Kieren102107262 <42164564+Kieren102107262@users.noreply.github.com>
2018-09-01 12:22:22 +1000

    Fixed folder name

---
+++ Cs Code/Model/ShipName.cs
@@ -0,0 +1,15 @@
+// The names of all of the ships in the game. Fairly self explanatory.
+public enum ShipName
+{
+    None = 0,
+    
+    Tug = 1,
+    
+    Submarine = 2,
+    
+    Destroyer = 3,
+    
+    Battleship = 4,
+    
+    AircraftCarrier = 5,
+}
