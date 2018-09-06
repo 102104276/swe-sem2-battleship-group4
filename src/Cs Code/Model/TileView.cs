1248ffcec6ea8de628926c986c6207a0f380de57
Kieren102107262 <42164564+Kieren102107262@users.noreply.github.com>
2018-09-06 20:01:23 +1000

    Finalised ShipName.cs and TileView.cs

--- src/Cs Code/Model/TileView.cs
+++ src/Cs Code/Model/TileView.cs
@@ -1,44 +1,29 @@
 
+// The values that are visable for a given tile.
+
 using Microsoft.VisualBasic;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 //using System.Data;
 using System.Diagnostics;
-/// <summary>
-/// The values that are visable for a given tile.
-/// </summary>
-public enum TileView
+namespace BattleShips
 {
-	/// <summary>
-	/// The viewer can see sea
-	/// </summary>
-	/// <remarks>
-	/// May be masking a ship if viewed via a sea adapter
-	/// </remarks>
-	Sea,
+	public enum TileView
+	{
+		// The viewer can see sea
+		// May be masking a ship if viewed via a sea adapter
+		Sea,
 
-	/// <summary>
-	/// The viewer knows that site was attacked but nothing
-	/// was hit
-	/// </summary>
-	Miss,
+		// The viewer knows that site was attacked but nothing
+		// was hit
+		Miss,
 
-	/// <summary>
-	/// The viewer can see a ship at this site
-	/// </summary>
-	Ship,
+		// The viewer can see a ship at this site
+		Ship,
 
-	/// <summary>
-	/// The viewer knows that the site was attacked and
-	/// something was hit
-	/// </summary>
-	Hit
+		// The viewer knows that the site was attacked and
+		// something was hit
+		Hit
+	}
 }
-
-//=======================================================
-//Service provided by Telerik (www.telerik.com)
-//Conversion powered by NRefactory.
-//Twitter: @telerik
-//Facebook: facebook.com/telerik
-//=======================================================
